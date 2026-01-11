c7e00d43a0d52194576fb0b9878670cce260767a
David Knight <david@knight.fm>
2026-01-11 11:48:51 -0800

    get wilder main worktree

--- aaa_install_scripts/setup_workspace.sh
+++ aaa_install_scripts/setup_workspace.sh
@@ -1,2 +1,13 @@
 mkdir -p /home/$USER/workspace
-git clone git@github.com:original-david-knight/davidknight.git /home/$USER/workspace/davidknight 
+
+if [ ! -d /home/$USER/workspace/davidknight ]; then
+  git clone git@github.com:original-david-knight/davidknight.git /home/$USER/workspace/davidknight
+fi
+
+if [ ! -d /home/$USER/workspace/wilder ]; then
+  git clone --bare git@github.com:original-david-knight/wilder.git /home/$USER/workspace/wilder
+fi
+
+if [ ! -d /home/$USER/workspace/wilder/main ]; then
+  git -C /home/$USER/workspace/wilder worktree add main main
+fi
